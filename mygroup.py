2e780a14c1ade6317a663fcf8c70534e14a3ce7d
jana luneva <jdemkiv@ya.ru>
2023-05-23 00:39:26 +0400

    fix lab1

--- mygroup.py
+++ mygroup.py
@@ -20,11 +20,23 @@ groupmates = [
 ]
 
 
+def search_middle():
+    search = float(input("средняя оценка"))
+    new_group = []
+    for students in groupmates:
+        middle = sum(students['marks']) / len(students['marks'])
+        if middle >= search:
+            students['middle'] = middle
+            new_group.append(students)
+    print_students(new_group)
+
+
 def print_students(students):
-    print(u"Имя".ljust(15), u"Фамилия".ljust(10), u"Экзамены".ljust(30), u"Оценки".ljust(20))
+    print(u"Имя".ljust(15), u"Фамилия".ljust(10), u"Экзамены".ljust(43), u"Оценки".ljust(20),
+          u"средняя оценка".ljust(20))
     for student in students:
-        print(student["name"].ljust(15), student["surname"].ljust(10), str(student["exams"]).ljust(30), str(student["marks"]).ljust(20))
+        print(student["name"].ljust(15), student["surname"].ljust(10), str(student["exams"]).ljust(40),
+              str(student["marks"]).ljust(25), str(student["middle"]).ljust(30))
 
 
-print_students(groupmates)
-
+search_middle()
